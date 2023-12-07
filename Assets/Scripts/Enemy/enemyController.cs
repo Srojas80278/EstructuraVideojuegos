3f0a69f985975641def885a73b06749ab1b9c6af
MikeZ184 <mike.herflo@gmail.com>
2023-12-07 02:04:15 -0600

    Update de la animacion en Y
    se arreglo los bugs de animaciones en Y

--- Assets/Scripts/Enemy/enemyController.cs
+++ Assets/Scripts/Enemy/enemyController.cs
@@ -12,21 +12,24 @@ public class EnemyController : MonoBehaviour
         aiLerp = GetComponent<AILerp>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector2 movementDirection = GetMovementDirection();
-
+        Debug.Log("Movement Direction: " + movementDirection);
         if (aiLerp != null)
         {
             if (Mathf.Abs(movementDirection.x) > Mathf.Abs(movementDirection.y))
             {
                 // Horizontal
+                Debug.Log("Horizontal Movement");
                 enemyAnimator.SetFloat("X", movementDirection.x);
                 enemyAnimator.SetFloat("Y", 0f);
             }
             else
             {
                 // Vertical
+                Debug.Log("Vertical Movement");
+                Debug.Log("X: " + movementDirection.x + ", Y: " + movementDirection.y);
                 enemyAnimator.SetFloat("X", 0f);
                 enemyAnimator.SetFloat("Y", movementDirection.y);
             }
@@ -37,16 +40,21 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+
     Vector2 GetMovementDirection()
     {
         if (aiLerp != null)
         {
             Vector3 aiVelocity = aiLerp.velocity;
-            return new Vector2(aiVelocity.x, aiVelocity.z).normalized;
+
+            // Utiliza aiVelocity.x y aiVelocity.y en lugar de aiVelocity.x y aiVelocity.z
+            return new Vector2(aiVelocity.x, aiVelocity.y).normalized;
         }
         else
         {
             return Vector2.zero;
         }
     }
+
+
 }
